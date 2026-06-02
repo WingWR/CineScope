fe40578de2d338a5bb9c8bb1d8c3cca2ea3cf01f
Wing_WR <wingwr998@gmail.com>
2026-06-02 16:14:16 +0800

    delete: 删除多余文件

--- frontend/src/api/recommendations.ts
+++
@@ -1,8 +0,0 @@
-import { apiGet } from './client'
-import type { MovieListItem } from '../types/api'
-
-export function getRecommendations(movieId?: string | number) {
-  const path = movieId ? `/recommendations/${movieId}` : '/recommendations'
-  return apiGet<MovieListItem[]>(path)
-}
-
